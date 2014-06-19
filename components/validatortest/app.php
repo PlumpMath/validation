15dde0d1279807878984f453a152559a62acd39e
William Stein <williamstein92@gmail.com>
2014-06-19 17:34:15 -0400

    stuff

--- components/validatortest/app.php
+++ components/validatortest/app.php
@@ -1,19 +1,12 @@
 <?php
 
-class ValidatorTest
+class ValidatorTest extends Factory
 {
   static $instancebin;
 
   public $name;
   public $test;
   public $message;
-
-  public function __construct($name, $test, $errormessage)
-  {
-    $this->name     = $name;
-    $this->test     = $test;
-    $this->message  = $errormessage;
-  }
 
   public function run($values)
   {
@@ -23,16 +16,5 @@ class ValidatorTest
   public function buildMessage($values)
   {
     return vsprintf($this->message, $values);
-  }
-
-  static function make($name, $test, $errormessage)
-  {
-    $me = get_called_class();
-    return static::$instancebin[$name] = new $me($name, $test, $errormessage);
-  }
-
-  static function get($name)
-  {
-    return static::$instancebin[$name];
   }
 }
